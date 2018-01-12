3704fdb584941b328f43b5086d805b3d8ec54f01
lemon <422896102@qq.com>
2018-01-12 17:24:11 +0800

    *

--- deal/数据处理.py
+++ deal/数据处理.py
@@ -1,46 +1,69 @@
 import pymongo
 import configparser
 import json
+import pymysql
 
 class dealSpider(object):
 
     top = [
-            ("ylct","饮料冲调"),
-            ("mc","茗茶"),
-            ("pplj","品牌礼券"),
-            ("dftc","地方特产"),
-            ("jksp","进口食品"),
-            ("lytw","粮油调味"),
-            ("xxsp","休闲食品"),
-            ("zwmj","中外名酒"),
-            ("xxsg","新鲜水果"),
-            ("hxsc","海鲜水产"),
-            ("ldys","精选肉类"),
-            ("jxrl","冷饮冻食")
+            ("ylct","饮料冲调","https://list.jd.com/list.html?cat=1320,1585"),
+            ("mc","茗茶","https://list.jd.com/list.html?cat=1320,12202"),
+            ("pplj","品牌礼券","https://list.jd.com/list.html?cat=1320,2641"),
+            ("dftc","地方特产","https://list.jd.com/list.html?cat=1320,1581"),
+            ("jksp","进口食品","https://list.jd.com/list.html?cat=1320,5019"),
+            ("lytw","粮油调味","https://list.jd.com/list.html?cat=1320,1584"),
+            ("xxsp","休闲食品","https://list.jd.com/list.html?cat=1320,1583"),
+            ("zwmj","中外名酒","https://list.jd.com/list.html?cat=12259,12260"),
+            ("xxsg","新鲜水果","https://list.jd.com/list.html?cat=12218,12221"),
+            ("hxsc","海鲜水产","https://list.jd.com/list.html?cat=12218,12222"),
+            ("ldys","精选肉类","https://list.jd.com/list.html?cat=12218,13581"),
+            ("jxrl","冷饮冻食","https://list.jd.com/list.html?cat=12218,13591")
         ]
 
     def __init__(self,file):
         #读取配置
         self.initConfig(file)
         #连接mongo
-        self.dbInit()
+        self.mongoInit()
+        #连接mysql
+        self.sqlInit()
+        
        
     def initConfig(self,file):
         conf = configparser.ConfigParser()
         conf.read(file)
         self.conf = conf
     
-    def getConfig(self,key):
-        return self.conf.get("mongo",key)
+    def getConfig(self,types,key):
+        return self.conf.get(types,key)
 
-    def dbInit(self):
+    #mysql
+    def sqlInit(self):
+        try:
+            self.link = pymysql.connect(
+                host = self.getConfig("mysql","host"),
+                port = int(self.getConfig("mysql","port")),
+                user = self.getConfig("mysql","user"),
+                password = self.getConfig("mysql","pwd"),
+                db = self.getConfig("mysql","dbname"),
+                charset = self.getConfig("mysql","charset")
+            )
+            # 使用cursor()方法获取操作游标
+            self.cur = self.link.cursor() 
+
+        except Exception as e:
+            print(e)
+            exit()
+
+    #mongodb
+    def mongoInit(self):
         
         try:
 
-            client = pymongo.MongoClient(host=self.getConfig("host"),port=int(self.getConfig("port")),\
-                                            username=self.getConfig("user"),password=self.getConfig("pwd"))
+            client = pymongo.MongoClient(host=self.getConfig("mongo","host"),port=int(self.getConfig("mongo","port")),\
+                                            username=self.getConfig("mongo","user"),password=self.getConfig("mongo","pwd"))
             #指向指定的数据库
-            self.db = client[self.getConfig("dbname")]
+            self.db = client[self.getConfig("mongo","dbname")]
             #指定集合
             #self.collection = db[collection]
 
@@ -51,13 +74,13 @@ class dealSpider(object):
     def setCollection(self,collection):
         self.collection = self.db[collection]
 
-    #无线级分类
+    #无线分类
     def generateTree(self,key,level):
         data = []
         level += 1
 
         for each in self.findData({"parent":key}):
-            res1 = {"name":each["top"]}
+            res1 = {"name":each["top"],"url":each["url"]}
             if each["top"] != each["parent"]:
                 children = self.generateTree(res1["name"],level)
                 if children:
@@ -77,17 +100,94 @@ class dealSpider(object):
             if "children" in value:
                 self.getTreeData(value["children"])
 
+    #mongo
     def findData(self,condition):
         return self.collection.find(condition)
 
+    #mysql
+    def findBysql(self,sql):
+        try: 
+            self.cur.execute(sql)
+            return self.cur.fetchall()
+        except Exception as e:  
+            raise e 
+
+    def insertBysql(self,sql):
+        try:
+            self.cur.execute(sql)
+             #提交  
+            self.link.commit()  
+            #返回主键id
+            return self.cur.lastrowid
+        except Exception as e:  
+            #错误回滚  
+            self.link.rollback()  
+            print(e) 
+
+    #写入顶级分类
+    def initTop(self):
+        for key,name,url in self.top:
+                sql = 'insert into yj_category(name,url,parent_id,level) \
+                        values ("'+name+'","'+url+'",0,1)'
+                print(deal.insertBysql(sql))
+
+    #转换json
     def toJson(self,list):
         return json.dumps(list, indent=2,ensure_ascii=False)
     
+    #show
+    def showAll(self):
+        for key,name,url in self.top:
+            deal.setCollection(key)
+            res = deal.generateTree(name,1)
+            print(self.toJson(res))
+
+    def showOne(self,key,name):
+        deal.setCollection(key)
+        res = deal.generateTree(name,1)
+        print(self.toJson(res))
+
+    #处理数据
+    def dealInsert(self,res,pid):
+        if res:
+            for item in res:
+                name = item["name"]
+                url = item["url"]
+                level = item["level"] 
+                #print(name+'-'+url+'-'+str(level)+'-'+str(pid))
+                sql = 'insert into yj_category(name,url,parent_id,level) \
+                        values ("'+name+'","'+url+'",'+str(pid)+','+str(level)+')'
+                lastid = self.insertBysql(sql)
+                print(lastid)
+                if "children" in item:
+                    self.dealInsert(item["children"],lastid)
+
+
+
+'''
+("ylct","饮料冲调","https://list.jd.com/list.html?cat=1320,1585"),
+("mc","茗茶","https://list.jd.com/list.html?cat=1320,12202"),
+("pplj","品牌礼券","https://list.jd.com/list.html?cat=1320,2641"),
+("dftc","地方特产","https://list.jd.com/list.html?cat=1320,1581"),
+("jksp","进口食品","https://list.jd.com/list.html?cat=1320,5019"),
+("lytw","粮油调味","https://list.jd.com/list.html?cat=1320,1584"),
+("xxsp","休闲食品","https://list.jd.com/list.html?cat=1320,1583"),
+("zwmj","中外名酒","https://list.jd.com/list.html?cat=12259,12260"),
+("xxsg","新鲜水果","https://list.jd.com/list.html?cat=12218,12221"),
+("hxsc","海鲜水产","https://list.jd.com/list.html?cat=12218,12222"),
+("jxrl","精选肉类","https://list.jd.com/list.html?cat=12218,13581"),
+("ldys","冷饮冻食","https://list.jd.com/list.html?cat=12218,13591")
+'''
 
 if __name__ == "__main__":
     deal = dealSpider('./config.ini')
-    #设置集合
-    deal.setCollection("mc")
-    res = deal.generateTree("茗茶",0)
-    #print(deal.toJson(res))
-    deal.getTreeData(res)
+
+    #deal.initTop()
+
+    deal.setCollection("ldys")
+    res = deal.generateTree("冷饮冻食",1)
+    #print(res)
+    deal.dealInsert(res,12)
+        
+    
+    
