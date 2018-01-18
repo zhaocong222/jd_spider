588ef3b7b89fd2fadb75b895c87bc39565192d32
lemon <422896102@qq.com>
2018-01-18 17:31:40 +0800

    *

--- jd_goods_spider/jd_goods_spider/spiders/goods.py
+++ jd_goods_spider/jd_goods_spider/spiders/goods.py
@@ -7,9 +7,6 @@ from scrapy_splash import SplashRequest
 
 class GoodsSpider(scrapy.Spider):
     name = 'goods'
-    allowed_domains = ['item.jd.com']
-    start_urls = ["https://list.jd.com/list.html?cat=12218,13591,13595&ev=1107_89465&sort=sort_rank_asc&trans=1&JL=3_%E5%88%86%E7%B1%BB_%E8%B4%A1%E4%B8%B8#J_crumbsBar"]
-
     
     def start_requests(self):
         file = os.getcwd() + '/../deal/leaf.json'
@@ -18,14 +15,16 @@ class GoodsSpider(scrapy.Spider):
             res = json.loads(f.read())
         
         for each in res:
-            yield SplashRequest(url=each["url"],callback=self.parse,meta={"name":each["name"],"id":each["id"]})
+            yield scrapy.Request(url=each["url"],callback=self.parselist,meta={"name":each["name"],"id":each["id"]})
           
-    #采集商品
-    def parse(self, response):
+            #yield SplashRequest(url=each["url"],callback=self.parselist,meta={"name":each["name"],"id":each["id"]})
+          
+    #采集商品列表
+    def parselist(self, response):
         
         res = response.xpath("//li[@class='gl-item']")
         for each in res:
-            item = JdGoodsSpiderItem()
+            
             img = each.xpath(".//div[@class='p-img']/a/img/@src").extract()
             if not img:
                 img = each.xpath(".//div[@class='p-img']/a/img/@data-lazy-img").extract()[0].strip()
@@ -33,19 +32,48 @@ class GoodsSpider(scrapy.Spider):
                 img = img[0].strip()
             url = each.xpath(".//div[@class='p-img']/a/@href").extract()[0].strip()
             title = each.xpath(".//div[@class='p-name']/a/em/text()").extract()[0].strip()
-            price = each.xpath(".//strong[@class='J_price']/i/text()").extract()[0]
+            #price = each.xpath(".//strong[@class='J_price']/i/text()").extract()[0]
             
-            item = {
+            data = {
                 "img" : img[2:],
-                "url" : url[2:],
+                "url" : "https://"+url[2:],
                 "title":title,
-                "price":price,
+            #    "price":price,
                 "price_plus":''
             }
             
             #会员价
-            price_plus = each.xpath(".//span[@class='price-plus-1']").extract()
-            if price_plus:
-                price_plus = price_plus[0].xpath("./em/text()").extract()
-                
-            print(price_plus)
+            #price_plus = each.xpath(".//span[@class='price-plus-1']").extract()
+            #if price_plus:
+            #    data["price_plus"] = each.xpath(".//span[@class='price-plus-1']/em/text()").extract()[0][1:]
+
+            name = response.meta["name"]
+            _id  = response.meta["id"]
+        
+            yield scrapy.Request(url=data["url"],callback=self.parseDetail,meta={"name":name,"id":_id,"goods":data})
+          
+    #采集商品详情
+    def parseDetail(self, response):
+        
+        name = response.meta["name"]
+        _id  = response.meta["id"]
+        data = response.meta["goods"]
+
+        #相册
+        res = response.xpath("//div[@id='spec-list']/ul/li")
+        gallery = []
+        for each in res:
+            src = each.xpath("./img/@src").extract()[0]
+            gallery.append(src[2:])
+
+        #介绍
+        #品牌
+        brand = response.xpath("//ul[@id='parameter-brand']li/a/text()").extract()[0]
+        
+            
+
+
+    
+
+
+    
