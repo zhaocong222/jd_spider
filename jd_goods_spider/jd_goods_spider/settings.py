bc04cb4df84f37dbaa4d9095ae502a149b50cb8d
unknown <422896102@qq.com>
2018-01-26 21:54:57 +0800

    *

--- jd_goods_spider/jd_goods_spider/settings.py
+++ jd_goods_spider/jd_goods_spider/settings.py
@@ -63,7 +63,8 @@ SPIDER_MIDDLEWARES = {
 DOWNLOADER_MIDDLEWARES = {
     #'jd_goods_spider.middlewares.JdGoodsSpiderDownloaderMiddleware': 543,
     #'jd_goods_spider.middlewares.RandomUserAgentMiddleware': 543,
-    'jd_goods_spider.middlewares.ProxyIpMiddleware':100,  
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware':123,
+    'jd_goods_spider.middlewares.ProxyIpMiddleware':125,  
     'scrapy_splash.SplashCookiesMiddleware': 723,
     'scrapy_splash.SplashMiddleware': 725,
     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
